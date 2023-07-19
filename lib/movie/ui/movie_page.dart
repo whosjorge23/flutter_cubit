f0298e557c92ff8946b9a7e1dd9083cea86b903f
Giorgio Giannotta <jorge_upa@msn.com>
2023-07-19 08:18:18 +0200

    Add Random movie

--- lib/movie/ui/movie_page.dart
+++ lib/movie/ui/movie_page.dart
@@ -1,10 +1,10 @@
+import 'dart:math';
+
 import 'package:cubit_freezed/movie/cubit/movie_cubit.dart';
 import 'package:cubit_freezed/movie/cubit/movie_state.dart';
 import 'package:cubit_freezed/movie/ui/movie_details_page.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-
-
 
 class MoviesPage extends StatefulWidget {
   @override
@@ -12,6 +12,8 @@ class MoviesPage extends StatefulWidget {
 }
 
 class _MoviesPageState extends State<MoviesPage> {
+  Random random = Random();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -30,24 +32,76 @@ class _MoviesPageState extends State<MoviesPage> {
             );
           } else if (state is LoadedState) {
             final movies = state.movies;
+            int randomMovie = random.nextInt(movies.length);
 
-            return ListView.builder(
-              itemCount: movies.length,
-              itemBuilder: (context, index) => Card(
-                child: GestureDetector(
-                  child: ListTile(
-                    title: Text(movies[index].title),
-                    leading: CircleAvatar(
-                      backgroundImage: NetworkImage(movies[index].urlImage),
+            return Column(
+              children: [
+                Expanded(
+                  child: GestureDetector(
+                    child: Stack(
+                      children: [
+                        SizedBox(
+                          width: MediaQuery.of(context).size.width,
+                          child: Image.network(
+                            movies[randomMovie].urlImage,
+                            fit: BoxFit.fitWidth,
+                          ),
+                        ),
+                        Positioned(
+                          bottom: 0,
+                          child: Container(
+                            width: MediaQuery.of(context).size.width,
+                            color: Colors.black.withOpacity(0.6),
+                            child: Padding(
+                              padding: const EdgeInsets.all(8.0),
+                              child: Text(
+                                movies[randomMovie].title,
+                                style: TextStyle(fontWeight: FontWeight.bold),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => MoviesDetailsPage(selectedMovie: movies[randomMovie]),
+                        ),
+                      );
+                    },
+                  ),
+                ),
+                Text(
+                  "Trending MovieList",
+                  style: TextStyle(fontWeight: FontWeight.bold),
+                ),
+                Expanded(
+                  flex: 2,
+                  child: ListView.builder(
+                    itemCount: movies.length,
+                    itemBuilder: (context, index) => Card(
+                      child: GestureDetector(
+                        child: ListTile(
+                          title: Text(movies[index].title),
+                          leading: CircleAvatar(
+                            backgroundImage: NetworkImage(movies[index].urlImage),
+                          ),
+                        ),
+                        onTap: () {
+                          Navigator.push(
+                            context,
+                            MaterialPageRoute(
+                              builder: (context) => MoviesDetailsPage(selectedMovie: movies[index]),
+                            ),
+                          );
+                        },
+                      ),
                     ),
                   ),
-                  onTap: () {
-                    Navigator.push(context, MaterialPageRoute(
-                      builder: (context) => MoviesDetailsPage(selectedMovie: movies[index]),
-                    ),);
-                  },
                 ),
-              ),
+              ],
             );
           } else {
             return Container();
